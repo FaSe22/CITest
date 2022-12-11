7bc10e71ceeafd1a1ebde1b6266c58ffb6788c70
sebastianfaber <sebastian@startup-werk.de>
2022-12-11 19:38:08 +0100

    add test for multiply function

--- main_test.go
+++ main_test.go
@@ -4,9 +4,15 @@ import(
 "testing"
 )
 
-func TestMain(t *testing.T){
+func TestSum(t *testing.T){
 	res := sum(1,1)
 	if res != 2 {
 		t.Errorf("sum was incorrect, got: %d want: %d.", res, 2)
 	}
 }
+
+func TestMultiply(t *testin.T){
+	res := multiply(2,3)
+	if res != 6 {
+		t.Errorf("multiply was incorrect, got: %d want %d.", res, 6)
+	}
